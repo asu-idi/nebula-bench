295ede71f0e72350e225ba2f2d0dc8a85fd65c6f
Qiaolin Yu <liin1211@outlook.com>
2022-03-15 12:42:17 +0800

    add MatchVertex case

--- nebula_bench/scenarios/match.py
+++ nebula_bench/scenarios/match.py
@@ -4,23 +4,16 @@ from nebula_bench.common.base import BaseScenario
 
 class BaseMatchScenario(BaseScenario):
     abstract = True
-    nGQL = "MATCH ()<-[e]-() \
-        RETURN e \
-        LIMIT 300;"
+    nGQL = "MATCH ()<-[e]-() RETURN e LIMIT 300;"
     csv_path = "social_network/dynamic/person_knows_person.csv"
     csv_index = [0]
 
 
 class MatchAllEdge(BaseMatchScenario):
     abstract = False
-    nGQL = "MATCH ()<-[e]-() \
-        RETURN e \
-        LIMIT 300;"
+    nGQL = "MATCH ()<-[e]-() RETURN e LIMIT 300;"
 
 
-# class MatchEdgeByType(BaseGoScenario):
-#     abstract = False
-#     nGQL = "MATCH ()-[e:know]->() \
-#         RETURN e \
-#         limit 3;"
-
+class MatchVertex(BaseMatchScenario):
+    abstract = False
+    nGQL = "MATCH (v) WHERE id(v) == {}  RETURN v;"
